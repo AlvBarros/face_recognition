16bcec401fdd533dac2b8d8cbad4f8f98996012f
AlvBarros <alv.barrosc@hotmail.com>
2019-03-16 11:34:39 -0300

    mostrar nro de arquivos e rostos encontrados

--- face.py
+++ face.py
@@ -10,16 +10,17 @@ for file in glob.glob("faces/*.jpeg"):
     txtfiles.append(file)
 
 print('Total files', len(txtfiles))
-
+faces_found = 0
 for file in txtfiles:
     img = cv.imread(file)
     gray = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces_found += len(faces)
     for (x,y,w,h) in faces:
         cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-        cv.imshow('img',img)
-        cv.waitKey(0)
-
+        # cv.imshow('img',img)
+        # cv.waitKey(0)
+print('Total faces', faces_found)
 cv.destroyAllWindows()
