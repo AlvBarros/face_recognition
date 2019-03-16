7e09a692c78d2e40c41e7a24254dc431fd46f38e
AlvBarros <alv.barrosc@hotmail.com>
2019-03-16 11:21:49 -0300

    mais imagens

--- face.py
+++ face.py
@@ -1,25 +1,23 @@
+import glob
 import numpy as np
 import cv2 as cv
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv.imread('a.jpg')
-img2 = cv.imread('b.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
-gray2 = cv.cvtColor(img2, cv.COLOR_BGRA2GRAY)
+txtfiles = [] 
+for file in glob.glob("faces/*.jpg"):
+    txtfiles.append(file)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-cv.imshow('img',img)
+print('Total files', len(txtfiles))
 
-faces2 = face_cascade.detectMultiScale(gray2, 1.3, 5)
-for (x,y,w,h) in faces2:
-    cv.rectangle(img2,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray2 = gray2[y:y+h, x:x+w]
-    roi_color2 = img[y:y+h, x:x+w]
-cv.imshow('img2',img2)
+for file in txtfiles:
+    img = cv.imread(file)
+    gray = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        cv.imshow('img',img)
+        cv.waitKey(0)
 
-cv.waitKey(0)
 cv.destroyAllWindows()
